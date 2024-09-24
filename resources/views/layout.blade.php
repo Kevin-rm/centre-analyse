23aa1551ccacd92df3e9e9ed4582188299c38a68
Kevin-rm <kevinramarozatovo@gmail.com>
2024-09-24 20:23:42 +0300

    refactor the layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -18,9 +18,7 @@
                 ],
                 urls: ["{{ asset("assets/css/fonts.min.css") }}"],
             },
-            active: function () {
-                sessionStorage.fonts = true;
-            },
+            active: () => { sessionStorage.fonts = true; },
         });
     </script>
 
@@ -30,196 +28,16 @@
 </head>
 <body>
 <div class="wrapper">
-    <!-- Sidebar -->
-    <div class="sidebar" data-background-color="dark">
-        <div class="sidebar-logo">
-            <!-- Logo Header -->
-            <div class="logo-header" data-background-color="dark">
-                <a href="" class="logo">
-                    <img
-                        src="assets/img/kaiadmin/logo_light.svg"
-                        alt="navbar brand"
-                        class="navbar-brand"
-                        height="20"
-                    />
-                </a>
-                <div class="nav-toggle">
-                    <button class="btn btn-toggle toggle-sidebar">
-                        <i class="gg-menu-right"></i>
-                    </button>
-                    <button class="btn btn-toggle sidenav-toggler">
-                        <i class="gg-menu-left"></i>
-                    </button>
-                </div>
-                <button class="topbar-toggler more">
-                    <i class="gg-more-vertical-alt"></i>
-                </button>
-            </div>
-            <!-- End Logo Header -->
-        </div>
-
-        <div class="sidebar-wrapper scrollbar scrollbar-inner">
-            <div class="sidebar-content">
-                <ul class="nav nav-secondary">
-                    <li class="nav-item active">
-                        <a
-                            data-bs-toggle="collapse"
-                            href="#dashboard"
-                            class="collapsed"
-                            aria-expanded="false"
-                        >
-                            <i class="fas fa-home"></i>
-                            <p>Dashboard</p>
-                            <span class="caret"></span>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
-    <!-- End Sidebar -->
+    @include("sidebar")
 
     <div class="main-panel">
-        <div class="main-header">
-            <div class="main-header-logo">
-                <!-- Logo Header -->
-                <div class="logo-header" data-background-color="dark">
-                    <a href="index.html" class="logo">
-                        
-                    </a>
-                    <div class="nav-toggle">
-                        <button class="btn btn-toggle toggle-sidebar">
-                            <i class="gg-menu-right"></i>
-                        </button>
-                        <button class="btn btn-toggle sidenav-toggler">
-                            <i class="gg-menu-left"></i>
-                        </button>
-                    </div>
-                    <button class="topbar-toggler more">
-                        <i class="gg-more-vertical-alt"></i>
-                    </button>
-                </div>
-                <!-- End Logo Header -->
-            </div>
-            <!-- Navbar Header -->
-            <nav class="navbar navbar-header navbar-header-transparent navbar-expand-lg border-bottom">
-                <div class="container-fluid">
-                    <nav class="navbar navbar-header-left navbar-expand-lg navbar-form nav-search p-0 d-none d-lg-flex">
-                        <div class="input-group">
-                            <div class="input-group-prepend">
-                                <button type="submit" class="btn btn-search pe-1">
-                                    <i class="fa fa-search search-icon"></i>
-                                </button>
-                            </div>
-                            <input type="text" placeholder="Search ..." class="form-control"/>
-                        </div>
-                    </nav>
-
-                    <ul class="navbar-nav topbar-nav ms-md-auto align-items-center">
-                        <li
-                            class="nav-item topbar-icon dropdown hidden-caret d-flex d-lg-none"
-                        >
-                            <a
-                                class="nav-link dropdown-toggle"
-                                data-bs-toggle="dropdown"
-                                href="#"
-                                role="button"
-                                aria-expanded="false"
-                                aria-haspopup="true"
-                            >
-                                <i class="fa fa-search"></i>
-                            </a>
-                            <ul class="dropdown-menu dropdown-search animated fadeIn">
-                                <form class="navbar-left navbar-form nav-search">
-                                    <div class="input-group">
-                                        <input
-                                            type="text"
-                                            placeholder="Search ..."
-                                            class="form-control"
-                                        />
-                                    </div>
-                                </form>
-                            </ul>
-                        </li>
-                        <li class="nav-item topbar-user dropdown hidden-caret">
-                            <a
-                                class="dropdown-toggle profile-pic"
-                                data-bs-toggle="dropdown"
-                                href="#"
-                                aria-expanded="false"
-                            >
-                                <span class="profile-username">
-                                  <span class="op-7">Hi,</span>
-                                  <span class="fw-bold">Hizrian</span>
-                                </span>
-                            </a>
-                            <ul class="dropdown-menu dropdown-user animated fadeIn">
-                                <div class="dropdown-user-scroll scrollbar-outer">
-                                    <li>
-                                        <div class="user-box">
-                                            
-                                            <div class="u-text">
-                                                <h4>Hizrian</h4>
-                                                <p class="text-muted">hello@example.com</p>
-                                                <a
-                                                    href="profile.html"
-                                                    class="btn btn-xs btn-secondary btn-sm"
-                                                >View Profile</a
-                                                >
-                                            </div>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div class="dropdown-divider"></div>
-                                        <a class="dropdown-item" href="#">My Profile</a>
-                                        <a class="dropdown-item" href="#">My Balance</a>
-                                        <a class="dropdown-item" href="#">Inbox</a>
-                                        <div class="dropdown-divider"></div>
-                                        <a class="dropdown-item" href="#">Account Setting</a>
-                                        <div class="dropdown-divider"></div>
-                                        <a class="dropdown-item" href="#">Logout</a>
-                                    </li>
-                                </div>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-            <!-- End Navbar -->
-        </div>
+        @include("header")
 
         <div class="container">
-            <div class="page-inner">
-            </div>
+            <div class="page-inner"></div>
         </div>
 
-        <footer class="footer">
-            <div class="container-fluid d-flex justify-content-between">
-                <nav class="pull-left">
-                    <ul class="nav">
-                        <li class="nav-item">
-                            <a class="nav-link" href="http://www.themekita.com">
-                                ThemeKita
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#"> Help </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#"> Licenses </a>
-                        </li>
-                    </ul>
-                </nav>
-                <div class="copyright">
-                    2024, made with <i class="fa fa-heart heart text-danger"></i> by
-                    <a href="http://www.themekita.com">ThemeKita</a>
-                </div>
-                <div>
-                    Distributed by
-                    <a target="_blank" href="https://themewagon.com/">ThemeWagon</a>.
-                </div>
-            </div>
-        </footer>
+        @include("footer")
     </div>
 </div>
 
