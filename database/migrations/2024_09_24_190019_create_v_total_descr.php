151f08b6e9514aafcf3c899bd412948e2c2f19d7
raina <rainahlandyony@gmail.com>
2024-09-26 08:53:13 +0200

    modification
    modification

---
+++ database/migrations/2024_09_24_190019_create_v_total_descr.php
@@ -0,0 +1,34 @@
+
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::statement("
+            CREATE OR REPLACE VIEW total_descr AS
+            SELECT 
+                SUM(a.total) AS sum_charge,
+                SUM(b.total_sum_variable) AS sum_total_sum_variable,
+                SUM(b.total_sum_fixe) AS sum_total_sum_fixe
+            FROM 
+                v_all_data_view a,
+                v_desc_total_par_charge b;
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::statement('DROP VIEW IF EXISTS total_descr');
+    }
+};
